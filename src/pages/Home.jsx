bc704a0edfb9a6983b63b5aaa2997b077a46799d
Darshil122 <darshildabhi2005@gmail.com>
2025-05-29 18:30:45 +0530

    Update Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,22 @@
 import Products from "./Products";
-
+import bgHome from "../assets/bgHome.png";
 const Home = () => {
-  return(
-    <>
-    <Products/>
-    </>
+  return (
+  <>
+  <div className="">
+    <img src={bgHome} alt="background-image" className="w-full h-[500px]" />
+    
+    <div className="absolute top-1/3 left-25 text-black max-w-md">
+      <h3 className="text-4xl font-bold mb-2">Online Shopping</h3>
+      <p className="text-lg">
+        Welcome to the future of online shopping! Discover amazing products at unbeatable prices.
+      </p>
+    </div>
+  </div>
+
+  <Products />
+</>
+
   );
 };
 
