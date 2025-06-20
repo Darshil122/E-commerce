750a1260acb6b7add5d5bc959993c7df6d41a79c
Darshil122 <darshildabhi2005@gmail.com>
2025-06-20 11:38:03 +0530

    Update file

--- src/Auth/UserAuth.jsx
+++ src/Auth/UserAuth.jsx
@@ -17,7 +17,7 @@ const UserAuth = ({ onLogin }) => {
 
   const formSubmit = async (data) => {
     try {
-      const response = await fetch("/api/login", {
+      const response = await fetch("/api/index", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({ ...data, action }),
