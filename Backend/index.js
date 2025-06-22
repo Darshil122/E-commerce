e34bcb224976d6db1831e80b1cb841a9f1fd8a73
Darshil122 <darshildabhi2005@gmail.com>
2025-06-22 14:31:06 +0530

    Create backend

---
+++ Backend/index.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+const app = express();
+
+app.use(express.json());
+app.use(cors())
+
+mongoose
+  .connect(
+    "mongodb+srv://darshildabhi:darshil2005@cluster0.gofcbib.mongodb.net/e-commerce?retryWrites=true&w=majority&appName=Cluster0"
+  )
+  .then(() => console.log("Database Connected"))
+  .catch((error) => console.log(error));
+
+const userSchema = mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    action:{type:String}
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("user", userSchema);
+
+app.post("/login", async (req, res) => {
+  const { name, email, password, action } = req.body;
+
+  try {
+    if (action === "Sign Up") {
+      const existingUser = await User.findOne({ email });
+      if (existingUser) {
+        return res.status(400).json({ error: "Email already exists" });
+      }
+
+      const data = await User.create({ name, email, password });
+      return res.status(201).json({ message: "User created successfully", data });
+    }
+
+    if (action === "Login") {
+      const user = await User.findOne({ email });
+      if (!user || user.password !== password) {
+        return res.status(400).json({ error: "Invalid email or password" });
+      }
+      return res.status(200).json({ message: "Login successful", user });
+    }
+
+    return res.status(400).json({ error: "Invalid action" });
+
+  } catch (err) {
+    console.error("Error:", err);
+    res.status(500).json({ error: "Internal Server Error" });
+  }
+});
+
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+app.listen(5000, () => console.log("Server Start"));
